7dcbc7d978b764cde184bead83ff47ee090360e6
GiftChrisJava <chrisjava77@gmail.com>
2024-07-29 21:01:58 +0200

    escapig

--- PhoneEmailExtractor.py
+++ PhoneEmailExtractor.py
@@ -2,10 +2,14 @@
 import pyperclip, re
 
 phoneRegex = re.compile(r'''(
-    (\d{3}|(\d{3}\))?                       #area code
-    (\s|-|.)?                               # seperator 
+    (\d{3}|\(\d{3}\))?                       #area code
+    (\s|-|\.)?                               # seperator 
     (\d{3})                                 # first 3 digits
     (\s|-|\.)                               # seperator
     (\d{4})                                 # last 4 digits
     (\s*(ext|x|ext.)\s*(\d{2,5}))?          #extension
     )''', re.VERBOSE)
+
+#TODO: create email regex
+#TODO : find matches in clipboard
+#TODO : copy results to the clipboard
