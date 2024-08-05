5bd56b072705723092b5d99f67be358403e7987e
GiftChrisJava <chrisjava77@gmail.com>
2024-08-05 22:56:59 +0200

    path

---
+++ files/tests.py
@@ -0,0 +1,9 @@
+# creating folders
+import os
+
+# os.makedirs("C:/Users/Gift Chris/Desktop/AdvancedPy/files/created")
+
+print(os.path.abspath("."))
+print(os.path.abspath(".\\Scripts"))
+print(os.path.isabs("C:/Users/Gift Chris/Desktop/AdvancedPy/files/created"))
+print(os.path.isabs("."))
