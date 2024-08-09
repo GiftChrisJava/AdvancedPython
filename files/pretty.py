0043aa75c5e3496c841dcfab0d896d65c41eeef6
GiftChrisJava <chrisjava77@gmail.com>
2024-08-09 15:18:06 +0200

    working with files

---
+++ files/pretty.py
@@ -0,0 +1,6 @@
+import pprint
+
+cats = [{'name': 'Zophie', 'desc': 'chubby'}, {'name': 'Pooka', 'desc':'fluffy'}]
+
+# pretty print the cats array
+print(pprint.pformat(cats))
--- files/tests.py
+++ files/tests.py
@@ -7,3 +7,16 @@ print(os.path.abspath("."))
 print(os.path.abspath(".\\Scripts"))
 print(os.path.isabs("C:/Users/Gift Chris/Desktop/AdvancedPy/files/created"))
 print(os.path.isabs("."))
+
+# list of filename string for each file 
+print(len(os.listdir("C:/Users/Gift Chris/Desktop")))
+
+
+# finding total number files storage
+totalSize = 0
+for filename in os.listdir("C:/Users/Gift Chris/Desktop"):
+    filenameSize = os.path.getsize(os.path.join("C:/Users/Gift Chris/Desktop", filename))
+    
+    totalSize = totalSize + filenameSize
+    
+print(totalSize / 1000, "mbs")
