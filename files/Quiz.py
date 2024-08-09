e7d97d913987c90eb0074c2627179fd21e251606
GiftChrisJava <chrisjava77@gmail.com>
2024-08-09 16:13:06 +0200

    Step 3: Create the Answer Options

--- files/Quiz.py
+++ files/Quiz.py
@@ -21,3 +21,36 @@ capitals = {
     'Montpelier', 'Virginia': 'Richmond', 'Washington': 'Olympia',
     'West Virginia': 'Charleston', 'Wisconsin': 'Madison', 'Wyoming': 'Cheyenne'
 }
+
+#  generate 35 quizz files
+for quizNum in range(3):
+    # TODO: Create the quiz and answer key files.
+    quizeFile = open("files/quiz/capitalquiz%s.txt" % (quizNum + 1), "w")
+    answerKeyFile = open("files/quiz/capitalquiz_answer%s.text" % (quizNum + 1), "w" )
+    
+    # TODO: Write out the header for the quiz.
+    quizeFile.write("Name:\n\nDate:\n\nPeriod:\n\n")
+    quizeFile.write((" " * 20)+ 'State Capitas Quiz (Form %s)' % (quizNum + 1))
+    quizeFile.write("\n\n")
+    
+    
+    # TODO: Shuffle the order of the states.
+    states = list(capitals.keys())
+    random.shuffle(states)
+    
+    # TODO: Loop through all 50 states, making a question
+    for question in range(5):
+        # get right and wrong answers.
+        correctAnswer = capitals[states[quizNum]]
+        wrongAnswers = list(capitals.values())
+        
+        # deleting the correct answer
+        del wrongAnswers[wrongAnswers.index(correctAnswer)]
+        
+        # selecting three random values from this list
+        wrongAnswers = random.sample(wrongAnswers, 3)
+        
+        answerOptions = wrongAnswers + [correctAnswer]
+        
+        # the answers need to be randomized
+        random.shuffle(answerOptions)
---
+++ files/me.py
@@ -0,0 +1,4 @@
+quizNum = "Jane"
+quizeFile = open("files/quiz/capitalquiz%s.txt" % (quizNum + "1"), "w")
+quizeFile.write("Oky")
+print(quizeFile)
